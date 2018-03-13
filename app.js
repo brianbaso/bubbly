85e1eae031b5441eb7ca24bbb4a99ac706511eb5
Brian Baso <brianbaso@gmail.com>
2018-03-12 22:05:22 -0400

    v0.1

---
+++ app.js
@@ -0,0 +1,115 @@
+var express		= require('express'),
+	app			= express(),
+	bodyParser 	= require('body-parser'),
+	mongoose	= require('mongoose')
+
+
+// 1. connect to local database
+mongoose.connect('mongodb://localhost/bubbly');
+
+// 2. set up the schema
+var barSchema = new mongoose.Schema({
+	name: String,
+	price: String,
+	type: String,
+	location: String,
+	bubblyScore: Number
+});
+
+// 3. complile into a schema
+var Bar = mongoose.model("Bar", barSchema);
+
+// Bar.create(
+// 	{name: "Blackbird", price: "$$", type: "🍺bar", location: "wynwood", bubblyScore: 4.6},
+// 	function(err, bar) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log('Created new bar!');
+// 			console.log(bar);
+// 		}
+// 	});
+
+// EJS shortcut
+app.set('view engine', 'ejs');
+
+// Serve static images, CSS, JS
+app.use(express.static('public'));
+
+// tell express to use bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+	res.render('landing');
+});
+
+// INDEX route: show all campgrounds
+app.get('/bars', function(req, res) {
+	if (req.query.search) {
+
+		Bar.find({}, function(err, allBars) {
+			if (err) {
+				console.log(err);
+			} else {
+				// data we pass through to the page
+				res.render('index', {bars: allBars});
+			}
+		});
+		
+	} else {
+	// get all the campgrounds from the db
+		Bar.find({}, function(err, allBars) {
+			if (err) {
+				console.log(err);
+			} else {
+				// data we pass through to the page
+				res.render('index', {bars: allBars});
+			}
+		});
+	}
+	// Old hard-coded array way.. pass in the bars array with {bars: bars} 
+	//res.render('bars', {bars: bars});
+});
+
+// form will send data to the POST route below
+app.get('/bars/new', function(req, res) {
+	res.render('new');
+});
+
+// SHOW route: show individual bar
+app.get('/bars/:id', function(req, res) {
+	// find the bar with provided id
+	Bar.findById(req.params.id, function(err, foundBar) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('show', {bar: foundBar});
+		}
+	});
+	// render show template with that bar
+});
+
+// POST route: so user can add bars & clubs
+app.post('/bars', function (req, res) {
+	// get data from form and add to bars array
+	var name = req.body.name;
+	var price = req.body.price;
+	var type = req.body.type;
+	var location = req.body.location;
+	var bubblyScore = req.body.bubblyScore;
+	var newBar = {name: name, price: price, type: type, location: location, bubblyScore: bubblyScore}
+
+	// Create a new bar, save it to the database
+	Bar.create(newBar, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			// redirect to bars page
+			res.redirect('/bars');
+		}
+	});
+});
+
+app.listen(3000, function() {
+	console.log('Let the bubbles ensue...');
+});
